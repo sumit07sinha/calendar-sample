52db86a9ec18c91071602289e3e22f294df41984
Sumit Sinha <51187812+sumit07sinha@users.noreply.github.com>
2020-02-04 15:02:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,3 @@ window.onload = function () {
 
     }
 }
-var para = document.createElement("P");                       // Create a <p> node
-var t = document.createTextNode("This is a paragraph.");      // Create a text node
-para.appendChild(t);                                          // Append the text to <p>
-document.getElementById("calendar").appendChild(para);
